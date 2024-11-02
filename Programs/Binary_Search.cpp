4b4d78b05e98c014ace5d5eaf1d7055c249871be
Muzammil <muzammil.ar.786@gmail.com>
2024-11-02 18:46:29 +0530

    Binary_Search

---
+++ Programs/Binary_Search.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// LeetCode
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start= 0, end= nums.size() - 1;
+        while(start <= end) {
+            int mid= start + (end - start)/2;
+
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                start= mid + 1;
+            else
+                end= mid - 1;
+        }
+        return -1;
+    }
+};
+
+// Coding Ninjas
+int search(vector<int>& nums, int target) {
+    int start= 0, end= nums.size() - 1;
+    while(start <= end) {
+        int mid= start + (end - start)/2;
+
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid] < target)
+            start= mid + 1;
+        else
+            end= mid - 1;
+    }
+    return -1;
+}
---
+++ Programs/Binary_Search.py
@@ -0,0 +1,30 @@
+import List
+
+# LeetCode
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end= 0, len(nums) - 1
+        while start <= end:
+            mid= start + (end - start)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start= mid + 1
+            else:
+                end= mid - 1
+        return -1
+
+# CodingNinjas
+def search(nums: List[int], target: int):
+    start, end= 0, len(nums) - 1
+    while start <= end:
+        mid= start + (end - start)//2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            start= mid + 1
+        else:
+            end= mid - 1
+    return -1
