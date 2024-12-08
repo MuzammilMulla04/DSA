f7681bdd439bc24353ffe82096a431ff5e0d48d8
Muzammil <muzammil.ar.786@gmail.com>
2024-12-08 19:27:38 +0530

    Reverse The Array

--- Programs/Reverse_The_Array.cpp
+++ Programs/Reverse_The_Array.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+/* CodingNinjas */
 void reverseArray(vector<int>& arr, int m) {
     for(int i= m+1, j= arr.size()-1; i<j; i++, j--)
         swap(arr[i], arr[j]);
 }
+
+/* GeeksForGeeks */
+class Solution {
+  public:
+    void reverseArray(vector<int>& arr) {
+        for(int i=0, j= arr.size()-1; i<j; i++, j--)
+            swap(arr[i], arr[j]);
+    }
+};
--- Programs/Reverse_The_Array.py
+++ Programs/Reverse_The_Array.py
@@ -1,8 +1,4 @@
-from os import *
-from sys import *
-from collections import *
-from math import *
-
+""" CodingNinjas """
 def reverseArray(arr, m):
     i= m+1
     j= len(arr) - 1
@@ -14,3 +10,12 @@ def reverseArray(arr, m):
         j -= 1
         
     return arr
+
+""" GeeksForGeeks """
+class Solution:
+    def reverseArray(self, arr):
+        i, j= 0, len(arr)-1
+        while i < j:
+            arr[i], arr[j]= arr[j], arr[i]
+            i += 1
+            j -= 1
